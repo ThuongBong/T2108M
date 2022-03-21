bda3594bee5fa537b190b401a7db4829879e9fbd
ThuongBong <yenpham2114@gmail.com>
2022-03-21 10:50:38 +0700

    commit triangle

---
+++ src/triangle/Main.java
@@ -0,0 +1,9 @@
+package triangle;
+
+public class Main {
+    public static void main(String[] args){
+    Triangle myTriagle = new Triangle();
+    myTriagle.chuvi();
+    myTriagle.dienTich();
+    }
+}
