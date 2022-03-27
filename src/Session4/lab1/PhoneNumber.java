06c1d3b875136be2c5bb6a0ede74facdfc700070
ThuongBong <yenpham2114@gmail.com>
2022-03-27 17:54:38 +0700

    commit code lab1

---
+++ src/Session4/lab1/PhoneNumber.java
@@ -0,0 +1,17 @@
+package Session4.lab1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PhoneNumber {
+    public String name;
+
+    public PhoneNumber(String name, String phone) {
+        this.name = name;
+        this.phone.add(phone);
+    }
+
+    ArrayList<String> phone = new ArrayList<String>();
+
+
+}
